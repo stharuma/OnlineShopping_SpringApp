30807c09349636dbbe4820f3495142bbc7b51ee1
stharuma <stharuma@gmail.com>
2018-07-25 13:02:07 -0400

    added @requestParam

--- onlineShopping_client/src/main/java/com/nj/stharuma/onlineShopping_client/controller/SMainController.java
+++ onlineShopping_client/src/main/java/com/nj/stharuma/onlineShopping_client/controller/SMainController.java
@@ -2,14 +2,26 @@ package com.nj.stharuma.onlineShopping_client.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SMainController {
-	@RequestMapping(value= {"/", "/home", "/index"})
+	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("main");
-		mv.addObject("greeting",  "Welcome to Spring MVC");
+		mv.addObject("greeting", "Welcome to Spring MVC");
+		return mv;
+	}
+
+	@RequestMapping(value = "/test")
+	// public ModelAndView test(@RequestParam("test") String greeting ) {
+	public ModelAndView test(@RequestParam(value="test", required=false) String greeting) {
+		if(greeting==null) {
+			greeting ="Hello There";
+		}
+		ModelAndView mv = new ModelAndView("main");
+		mv.addObject("greeting", greeting);
 		return mv;
 	}
 
